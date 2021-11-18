7e9ef74370c7f08e6847947aa2ee811dd48d6f9e
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-11-17 20:15:10 -0800

    use fetchone

--- helpers.py
+++ helpers.py
@@ -28,8 +28,8 @@ async def get_top_100(role: Role, amount: int=100):
     return list(q)
 
 async def get_total_players():
-    q = await sql.fetchall("SELECT COUNT(*) FROM players")
-    return q[0][0]
+    q = await sql.fetchone("SELECT COUNT(*) FROM players")
+    return q[0]
 
 async def get_mmr(player_id: str):
     q = await sql.fetchall("SELECT ranking_scout, ranking_heavy, ranking_commander, ranking_medic, ranking_ammo FROM `players` WHERE `player_id` = %s", (player_id))
