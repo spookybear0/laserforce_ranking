78b9cb69d9798503c990b52cadd5894ddbb1eac7
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-12-13 15:05:43 -0800

    rip bozo

--- logs.py
+++ logs.py
@@ -6,17 +6,29 @@ logger = logging.getLogger("general")
 elo_logger = logging.getLogger("elo cron")
 player_logger = logging.getLogger("player cron")
 
-logger.setLevel(logging.DEBUG)
-elo_logger.setLevel(logging.DEBUG)
-player_logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler("general.log", mode="w")
+handler.setFormatter(fmt)
+logger.addHandler(handler)
 
-logger.addHandler(logging.FileHandler("general.log", mode="w").setFormatter(fmt))
-elo_logger.addHandler(logging.FileHandler("elo_cron.log", mode="w").setFormatter(fmt))
-player_logger.addHandler(logging.FileHandler("player_cron.log", mode="w").setFormatter(fmt))
+handler = logging.FileHandler("elo_cron.log", mode="w")
+handler.setFormatter(fmt)
+elo_logger.addHandler(handler)
 
-logger.addHandler(logging.StreamHandler().setFormatter(fmt))
-elo_logger.addHandler(logging.StreamHandler().setFormatter(fmt))
-player_logger.addHandler(logging.StreamHandler().setFormatter(fmt))
+handler = logging.FileHandler("player_cron.log", mode="w")
+handler.setFormatter(fmt)
+player_logger.addHandler(handler)
+
+handler = logging.StreamHandler()
+handler.setFormatter(fmt)
+logger.addHandler(handler)
+
+handler = logging.StreamHandler()
+handler.setFormatter(fmt)
+elo_logger.addHandler(handler)
+
+handler = logging.StreamHandler()
+handler.setFormatter(fmt)
+player_logger.addHandler(handler)
     
 def get_log(log: str):
     return open(f"{log}.log", "r").read()
