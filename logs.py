7f70f6cea3d84a85c638b832651ceab563957d26
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-12-13 14:43:51 -0800

    set fmt in right spot

--- logs.py
+++ logs.py
@@ -10,17 +10,13 @@ logger.setLevel(logging.DEBUG)
 elo_logger.setLevel(logging.DEBUG)
 player_logger.setLevel(logging.DEBUG)
 
-logger.setFormatter(fmt)
-elo_logger.setFormatter(fmt)
-player_logger.setFormatter(fmt)
+logger.addHandler(logging.FileHandler("general.log", mode="w").setFormatter(fmt))
+elo_logger.addHandler(logging.FileHandler("elo_cron.log", mode="w").setFormatter(fmt))
+player_logger.addHandler(logging.FileHandler("player_cron.log", mode="w").setFormatter(fmt))
 
-logger.addHandler(logging.FileHandler("general.log", mode="w"))
-elo_logger.addHandler(logging.FileHandler("elo_cron.log", mode="w"))
-player_logger.addHandler(logging.FileHandler("player_cron.log", mode="w"))
-
-logger.addHandler(logging.StreamHandler())
-elo_logger.addHandler(logging.StreamHandler())
-player_logger.addHandler(logging.StreamHandler())
+logger.addHandler(logging.StreamHandler().setFormatter(fmt))
+elo_logger.addHandler(logging.StreamHandler().setFormatter(fmt))
+player_logger.addHandler(logging.StreamHandler().setFormatter(fmt))
     
 def get_log(log: str):
     return open(f"{log}.log", "r").read()
