1ab8c5becf644789c370e08230ecc4219bdb1990
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2021-12-13 16:54:55 -0800

    remove hook and use easier approach

--- logs.py
+++ logs.py
@@ -1,26 +1,15 @@
 import logging
 
-def hook(hookfunc, oldfunc):
-    def hooked(*args, **kwargs):
-        hookfunc()
-        return oldfunc(*args, **kwargs)
-    return hooked
-
-def log_hook():
-    # make it one line
-    open("general.log", "w+")
-    open("elo_cron.log", "w+")
-    open("player_cron.log", "w+")
+# reset
+open("general.log", "w")
+open("elo_cron.log", "w")
+open("player_cron.log", "w")
 
 fmt = logging.Formatter("%(name)s :: %(asctime)s - %(levelname)s: %(message)s")
 
 logger = logging.getLogger("general")
 elo_logger = logging.getLogger("elo cron")
 player_logger = logging.getLogger("player cron")
-
-logger._log = hook(log_hook, logger._log)
-elo_logger._log = hook(log_hook, elo_logger._log)
-player_logger._log = hook(log_hook, player_logger._log)
 
 handler = logging.FileHandler("general.log", mode="a")
 handler.setFormatter(fmt)
@@ -53,4 +42,5 @@ handler.setLevel(logging.ERROR)
 player_logger.addHandler(handler)
     
 def get_log(log: str):
-    return open(f"{log}.log", "r").read()
+    with open(f"{log}.log", "r") as f:
+        return f.readlines()[-1]
