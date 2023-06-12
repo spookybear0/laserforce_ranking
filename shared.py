c94c44611c5894d64d852a733a7da7c8322902f4
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2023-06-12 13:51:37 -0700

    actually fix cors

--- shared.py
+++ shared.py
@@ -5,6 +5,7 @@ from sentry_sdk.integrations.sanic import SanicIntegration
 from sanic_jinja2 import SanicJinja2
 from config import config
 import sentry_sdk
+from sanic_cors import CORS
 
 sentry_sdk.init(
     dsn=config["sentry_dsn"],
@@ -19,6 +20,7 @@ sentry_sdk.init(
 )
 
 app = Sanic("laserforce_rankings")
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.ctx.jinja = SanicJinja2(app, loader=jinja2.FileSystemLoader("./assets/html"), pkg_path="assets/html", extensions=["jinja2.ext.loopcontrols"])
 app.ctx.sql = MySQLPool()
 app.ctx.config = config
