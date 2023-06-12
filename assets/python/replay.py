4ef44e6f686a6a02e663bdf3d1c550acc1bd69a9
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2023-06-12 14:29:32 -0700

    cors pt9999

--- assets/python/replay.py
+++ assets/python/replay.py
@@ -276,7 +276,7 @@ async def main_loop(game):
 
 
 async def main():
-    resp = await request(f"/api/game/{js.game_id}", headers={"Access-Control-Allow-Origin": "*"})
+    resp = await request(f"/api/game/{js.game_id}", headers={"Access-Control-Allow-Origin": "*", "mode": "cors"})
     game = await resp.json()
     await main_loop(game)
     
