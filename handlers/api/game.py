38e542a163fc5400d72bdccba5cd989d5ec36222
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2024-03-05 06:43:34 -0800

    fix: allow laserball games to be downloaded correctly with the api

--- handlers/api/game.py
+++ handlers/api/game.py
@@ -8,17 +8,17 @@ from sanic import exceptions, response
 from sanic.log import logger
 from helpers.statshelper import sentry_trace
 
-@app.get("/api/game/<type:str>/<id:int>")
+@app.get("/api/game/<type_:str>/<id:int>")
 @sentry_trace
-async def api_game(request: Request, type: str, id: int) -> str:
+async def api_game(request: Request, type_: str, id: int) -> str:
     """
     Returns a tdf file for the game
     """
     logger.info(f"Game {id} requested from api")
 
-    if type == "sm5":
+    if type_ == "sm5":
         game = await SM5Game.filter(id=id).first()
-    elif type == "lb":
+    elif type_ == "lb":
         game = await LaserballGame.filter(id=id).first()
     else:
         raise exceptions.BadRequest("Invalid game type!", status_code=400)
@@ -26,20 +26,22 @@ async def api_game(request: Request, type: str, id: int) -> str:
     if game is None:
         raise exceptions.NotFound("Game not found!", status_code=404)
     
+    full_type_name = "sm5" if type_ == "sm5" else "laserball"
+    
     # return the tdf file
-    return await response.file(f"sm5_tdf/{game.tdf_name}", filename=game.tdf_name)
+    return await response.file(f"{full_type_name}_tdf/{game.tdf_name}", filename=game.tdf_name)
 
-@app.get("/api/game_json/<type:str>/<id:int>")
+@app.get("/api/game_json/<type_:str>/<id:int>")
 @sentry_trace
-async def api_game_json(request: Request, type: str, id: int) -> str:
+async def api_game_json(request: Request, type_: str, id: int) -> str:
     """
     This is meant for the web frontend only to request a game from the api
     """
     logger.info(f"Game {id} requested from api")
 
-    if type == "sm5":
+    if type_ == "sm5":
         game = await SM5Game.filter(id=id).first()
-    elif type == "lb":
+    elif type_ == "lb":
         game = await LaserballGame.filter(id=id).first()
     else:
         raise exceptions.BadRequest("Invalid game type!", status_code=400)
