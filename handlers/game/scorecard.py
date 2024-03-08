4df2f5f4238c2426943bdc118f51b8d5bef1f32e
SpookyBear0 <50897638+spookybear0@users.noreply.github.com>
2024-03-07 20:15:21 -0800

    stylistic edits for PR #5

--- handlers/game/scorecard.py
+++ handlers/game/scorecard.py
@@ -21,7 +21,7 @@ async def get_laserballstats(entity):
 
 
 def _stat_str(key: str, value: str, condition: bool = True):
-    return [{'key': key, 'value': value}] if condition else []
+    return [{"key": key, "value": value}] if condition else []
 
 
 def _stat(key: str, value: int, condition: bool = True):
@@ -31,7 +31,7 @@ def _stat(key: str, value: int, condition: bool = True):
 def _players_in_team(all_players: List[dict], team_index: int):
     """Returns subset of the list of players - only those in the given team."""
     return [
-        player for player in all_players if player['team'] == team_index
+        player for player in all_players if player["team"] == team_index
     ]
 
 
@@ -80,54 +80,54 @@ async def scorecard(request: Request, type: str, id: int, entity_end_id: int) ->
         kd_ratio = stats.shot_opponent / stats.times_zapped if stats.times_zapped > 0 else 1
 
         main_stats = (
-              _stat('Score', entity_end.score) +
-              _stat('Lives left', stats.lives_left) +
-              _stat('Shots left', stats.shots_left, entity_start.role != IntRole.AMMO) +
-              _stat('Shots fired', stats.shots_fired) +
-              _stat_str('Accuracy', '%.2f%%' % (accuracy * 100)) +
-              _stat_str('K/D', '%.2f' % kd_ratio) +
-              _stat('Missiles fired', stats.missile_hits, can_missile) +
-              _stat('Missiled team', stats.missiled_team, can_missile) +
-              _stat('Nukes detonated', stats.nukes_detonated, can_nuke) +
-              _stat('Nukes canceled', stats.nuke_cancels, can_nuke) +
-              _stat('Medic hits', stats.medic_hits)
+              _stat("Score", entity_end.score) +
+              _stat("Lives left", stats.lives_left) +
+              _stat("Shots left", stats.shots_left, entity_start.role != IntRole.AMMO) +
+              _stat("Shots fired", stats.shots_fired) +
+              _stat_str("Accuracy", "%.2f%%" % (accuracy * 100)) +
+              _stat_str("K/D", "%.2f" % kd_ratio) +
+              _stat("Missiles fired", stats.missile_hits, can_missile) +
+              _stat("Missiled team", stats.missiled_team, can_missile) +
+              _stat("Nukes detonated", stats.nukes_detonated, can_nuke) +
+              _stat("Nukes canceled", stats.nuke_cancels, can_nuke) +
+              _stat("Medic hits", stats.medic_hits)
         )
 
         entity_starts: List[EntityStarts] = game.entity_starts
         player_entities = [
-            player for player in list(entity_starts) if player.type == 'player'
+            player for player in list(entity_starts) if player.type == "player"
         ]
 
         # TODO: This could be done in a single query with a nifty group_by,
-        #   but group_by doesn't seem to work well with JSON columns.
+        #   but group_by doesn"t seem to work well with JSON columns.
 
         all_players = ([
             {
-                'name': player.name,
-                'team': (await player.team).index,
-                'entity_end_id': (await EntityEnds.filter(entity=player.id).first()).id,
-                'role': player.role,
-                'score': (await EntityEnds.filter(entity=player.id).first()).score,
-                'you_zapped': await _count_zaps(game, entity_start.entity_id, player.entity_id),
-                'zapped_you': await _count_zaps(game, player.entity_id, entity_start.entity_id),
-                'you_missiled': await _count_missiles(game, entity_start.entity_id, player.entity_id),
-                'missiled_you': await _count_missiles(game, player.entity_id, entity_start.entity_id),
+                "name": player.name,
+                "team": (await player.team).index,
+                "entity_end_id": (await EntityEnds.filter(entity=player.id).first()).id,
+                "role": player.role,
+                "score": (await EntityEnds.filter(entity=player.id).first()).score,
+                "you_zapped": await _count_zaps(game, entity_start.entity_id, player.entity_id),
+                "zapped_you": await _count_zaps(game, player.entity_id, entity_start.entity_id),
+                "you_missiled": await _count_missiles(game, entity_start.entity_id, player.entity_id),
+                "missiled_you": await _count_missiles(game, player.entity_id, entity_start.entity_id),
             } for player in player_entities
         ])
-        all_players.sort(key=lambda x: x['score'], reverse=True)
+        all_players.sort(key=lambda x: x["score"], reverse=True)
 
         teams = [
             {
-                'name': 'Earth Team',
-                'class_name': 'earth',
-                'score': await game.get_green_score(),
-                'players': _players_in_team(all_players, 1)
+                "name": "Earth Team",
+                "class_name": "earth",
+                "score": await game.get_green_score(),
+                "players": _players_in_team(all_players, 1)
             },
             {
-                'name': 'Fire Team',
-                'class_name': 'fire',
-                'score': await game.get_red_score(),
-                'players': _players_in_team(all_players, 0)
+                "name": "Fire Team",
+                "class_name": "fire",
+                "score": await game.get_red_score(),
+                "players": _players_in_team(all_players, 0)
             },
         ]
 
