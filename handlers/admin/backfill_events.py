4712c0d1a45102f75e8e25534bbf892e354d4528
Ebo <ebo@ebomike.com>
2024-03-20 21:33:33 -0700

    Just get all events and migrate them. We don't need to get them via the games.

--- handlers/admin/backfill_events.py
+++ handlers/admin/backfill_events.py
@@ -1,11 +1,7 @@
-import time
-
 from sanic import Request
 from sanic.log import logger
 
 from db.game import Events
-from db.laserball import LaserballGame
-from db.sm5 import SM5Game
 from helpers.tdfhelper import get_arguments_from_event
 from shared import app
 from utils import admin_only
@@ -18,61 +14,24 @@ async def backfill_events(request: Request) -> str:
 
     await response.send("<html><body><H1>Updating</H1>\n")
 
-    # Convert every game one by one.
-    sm5games = await SM5Game.all()
+    # Get all events. We only need those without an action. If it has one, it has already been migrated.
+    events = await Events.filter(action="").all()
+    updated_event_count = await _update_events(events)
 
-    await _update_all_games(response, sm5games,
-                            "SM5",
-                            lambda game: game.start_time,
-                            lambda game: game.id,
-                            lambda game: game.events.all()
-                            )
-
-    lb_games = await LaserballGame.all()
-
-    await _update_all_games(response, lb_games,
-                            "Laserball",
-                            lambda game: game.start_time,
-                            lambda game: game.id,
-                            lambda game: game.events.all()
-                            )
-
-    await response.send("<h2>All done.</h2>\n</body></html>")
+    await response.send(f"<h2>All done.</h2>\n{updated_event_count} events updated.\n</body></html>")
 
     return ""
 
 
-async def _update_all_games(response, games, game_type: str, time_fetcher, id_fetcher, events_fetcher):
-    count = 0
-    total = len(games)
-
-    for game in games:
-        log_string = f"({count}/{total}) Updating {game_type} game {time_fetcher(game).strftime('%A, %B %d at %I:%M %p')} with ID {id_fetcher(game)}"
-        await response.send(f"{log_string}<br>\n")
-
-        update_start = time.perf_counter()
-        await _update_events(await events_fetcher(game))
-        update_time = time.perf_counter() - update_start
-        logger.info(f"{log_string} - done in {update_time * 1000}ms)")
-
-        count += 1
-
-
-async def _update_events(events: list[Events]):
-    events_to_update = []
+async def _update_events(events: list[Events]) -> int:
     for event in events:
-        # If this event already has an action, then it has previously been
-        # converted to the new format already, so we don't need to do anything
-        # here.
-        if event.action:
-            continue
-
         semantic_arguments = get_arguments_from_event(event.arguments)
         event.entity1 = semantic_arguments["entity1"]
         event.entity2 = semantic_arguments["entity2"]
         event.action = semantic_arguments["action"]
-        events_to_update.append(event)
 
-    if events_to_update:
-        await Events.bulk_update(events_to_update, fields=['entity1', 'action', 'entity2'])
-        logger.info('Updating %d events' % len(events_to_update))
+    if events:
+        await Events.bulk_update(events, fields=['entity1', 'action', 'entity2'])
+        logger.info('Updating %d events' % len(events))
+
+    return len(events)
