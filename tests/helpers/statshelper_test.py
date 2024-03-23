b2ed06e56f24973d0c256e405c3f2030531ae0a3
Michael A. Krehan <github@ebomike.com>
2024-03-23 13:29:42 -0700

    Clean up the test environment after a test

--- tests/helpers/statshelper_test.py
+++ tests/helpers/statshelper_test.py
@@ -2,12 +2,15 @@ import unittest
 
 from db.sm5 import SM5Game
 from helpers.statshelper import count_zaps
-from tests.helpers.environment import setup_test_database, ENTITY_ID_1, ENTITY_ID_2, get_sm5_game_id
+from tests.helpers.environment import setup_test_database, ENTITY_ID_1, ENTITY_ID_2, get_sm5_game_id, teardown_test_database
 
 
 class TestStringMethods(unittest.IsolatedAsyncioTestCase):
     async def asyncSetUp(self):
         await setup_test_database()
+
+    async def asyncTearDown(self):
+        await teardown_test_database()
 
     async def test_count_zaps(self):
         game = await SM5Game.filter(id=get_sm5_game_id()).first()
