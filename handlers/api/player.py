c7224a02ec588d3fe41e41cd5df304ffffb11c3b
spookybear0 <50897638+spookybear0@users.noreply.github.com>
2024-08-17 00:22:55 -0700

    rfid reader support

--- handlers/api/player.py
+++ handlers/api/player.py
@@ -4,11 +4,12 @@ from sanic import response
 from db.player import Player
 from helpers.statshelper import sentry_trace
 from shared import app
+from typing import Union
 
 
 @app.post("/api/player/<identifier:str>")
 @sentry_trace
-async def api_player(request: Request, identifier: str) -> str:
+async def api_player(request: Request, identifier: Union[str, int]) -> str:
     # get information about the player
 
     stats = request.json.get("stats")  # if true, return stats
@@ -19,14 +20,23 @@ async def api_player(request: Request, identifier: str) -> str:
     if recent_games is None:
         recent_games = False
 
+
     if identifier is None:
         return response.json({"error": "Invalid identifier"}, status=401)
+
 
     player = await Player.filter(codename=identifier).first()
 
     if player is None:
         player = await Player.filter(player_id=identifier).first()
         if player is None:
-            return response.json({"error": "Player not found"}, status=404)
+            try:
+                rfid_tag = int(identifier)
+            except ValueError:
+                return response.json({"error": "Player not found"}, status=404)
+            # get from rfid_tags
+            player = await Player.filter(rfid_tags__contains=[rfid_tag]).first()
+            if player is None:
+                return response.json({"error": "Player not found"}, status=404)
 
     return response.json(await player.to_dict(include_stats=stats, include_recent_games=recent_games))
