0a9f63c40413a4b488d3ec2744e809c370818938
Ebo <ebo@ebomike.com>
2024-12-26 11:45:16 -0800

    Added bar graph with SM5 games per role. The graph differentiates between ranked games and all games. That helps understand the radar graph (ranked only) and the "favorite role" at the bottom (all games).

---
+++ handlers/util/upload_tdf_backup.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+
+from sanic import Request, exceptions, response
+from shared import app
+from utils import get_post
+from helpers.tdfhelper import parse_sm5_game, parse_laserball_game
+from helpers.statshelper import sentry_trace
+from sanic.log import logger
+
+@app.get("/util/auto_upload_dl_DO_NOT_SUBMIT")
+async def auto_upload_dl(request: Request) -> str:
+    return response.file("./upload_scripts/upload.bat")
+
+@app.post("/util/upload_tdf_DO_NOT_SUBMIT")
+@sentry_trace
+async def auto_upload(request: Request) -> str:
+    logger.info("Uploading TDF")
+
+    data = get_post(request)
+    type = data.get("type")
+    file = request.files.get("upload_file")
+
+    logger.debug(f"Type: {type}")
+    logger.debug(f"File: {file}")
+    
+    if file is None:
+        raise exceptions.BadRequest()
+
+    if type == "sm5":
+        target_path = "./sm5_tdf/" + file.name
+        _create_file_from_request(file, target_path)
+        await parse_sm5_game(target_path)
+    elif type == "laserball":
+        target_path = "./laserball_tdf/" + file.name
+        _create_file_from_request(file, target_path)
+        await parse_laserball_game(target_path)
+    else:
+        raise exceptions.BadRequest()
+    
+    logger.info("Uploaded TDF successfully!")
+
+    return response.text("Uploaded!")
+
+
+def _create_file_from_request(request_file, target_path: str):
+    """Reads the data from a request's file and stores it a local file.
+
+    Creates the path structure leading up to the target path if it doesn't
+    exist already.
+
+    Args:
+        request_file: The file from the HTTP request.
+        target_path: The path to store this file in. Can be relative.
+    """
+    filepath = Path(target_path)
+
+    # Create the directory if it doesn't exist already.
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+
+    # Copy the entire contents of the request file into the target file.
+    open(target_path, "wb").write(request_file.body)
