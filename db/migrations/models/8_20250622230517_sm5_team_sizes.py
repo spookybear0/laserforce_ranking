4a5ba9285862bd5a2b82679a6ed53b4a39f89120
Ebo <ebo@ebomike.com>
2025-06-23 21:58:07 -0700

    The team size fields should allow NULL for compatibility.

--- db/migrations/models/8_20250622230517_sm5_team_sizes.py
+++ db/migrations/models/8_20250622230517_sm5_team_sizes.py
@@ -3,8 +3,8 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        ALTER TABLE `sm5game` ADD `team1_size` INT NOT NULL;
-        ALTER TABLE `sm5game` ADD `team2_size` INT NOT NULL;"""
+        ALTER TABLE `sm5game` ADD `team1_size` INT NULL;
+        ALTER TABLE `sm5game` ADD `team2_size` INT NULL;"""
 
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
