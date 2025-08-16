3af4a8671dc20e414ba25f9725a1c31da15894f6
spookybear0 <50897638+spookybear0@users.noreply.github.com>
2025-08-15 18:03:34 -0700

    remove legacy replay code

--- handlers/game/replay.py
+++ handlers/game/replay.py
@@ -10,11 +10,7 @@ from utils import render_template
 @sentry_trace
 async def game_replay(request: Request, type: str, id: int) -> str:
     if type == "sm5":
-        # Uncomment this line to use the old-style replay.
-        # return await render_template(request, "game/replay_sm5.html", game_id=id)
         return await render_template(request, "game/replay.html", game_type=type, game_id=id)
     elif type in "laserball":
-        # Uncomment this line to use the old-style replay.
-        # return await render_template(request, "game/replay_laserball.html", game_id=id)
         return await render_template(request, "game/replay.html", game_type=type, game_id=id)
     raise exceptions.BadRequest("Invalid game type")
