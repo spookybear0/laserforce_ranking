6bb5530799660135618a25a59afe1ad35ecbd425
Ebo <ebo@ebomike.com>
2026-02-01 23:29:22 -0800

    Make the upload script configurable and more robust.
    - No duplicated logic
    - Handles errors on upload
    - Moves uploaded files into a folder and deletes uploaded files after 7 days

--- upload_scripts/upload.bat
+++ upload_scripts/upload.bat
@@ -1,11 +1,43 @@
+#@echo off
+setlocal
+
 :loop
-for %%i in (C:\laserball_tdfs\*) do (
-    curl -F "upload_file=@%%i" -F "type=laserball" https://laserforce.spoo.uk/util/upload_tdf
-    del %%i
+rem Define list of directories and types (format: "path;type")
+set "dirs="C:\temp\laserball_tdfs;laserball" "C:\temp\sm5_tdfs;sm5""
+set "upload_url="https://laserforce.spoo.uk/util/upload_tdf""
+
+for %%d in (%dirs%) do (
+    for /f "tokens=1,2 delims=;" %%a in ("%%~d") do (
+        call :process_dir "%%a" "%%b"
+    )
 )
-for %%i in (C:\sm5_tdfs\*) do (
-    curl -F "upload_file=@%%i" -F "type=sm5" https://laserforce.spoo.uk/util/upload_tdf
-    del "%%i"
-)
+
 timeout /t 60 > NUL
 goto loop
+
+:process_dir
+set "dir_path=%~1"
+set "type=%~2"
+set "uploaded_path=%dir_path%\uploaded"
+
+if not exist "%uploaded_path%" mkdir "%uploaded_path%"
+
+rem Iterate over files in the directory
+for %%f in ("%dir_path%\*") do (
+    echo Uploading "%%~nxf"...
+
+    curl -f -F "upload_file=@%%~f" -F "type=%type%" %upload_url%
+
+    if errorlevel 1 (
+        echo Error: Curl call failed for %%~nxf
+        timeout /t 10
+        exit
+    )
+
+    move /Y "%%~f" "%uploaded_path%" >nul
+)
+
+rem Delete files older than 7 days in uploaded folder
+forfiles /p "%uploaded_path%" /m * /d -7 /c "cmd /c del @path" 2>nul
+
+exit /b
